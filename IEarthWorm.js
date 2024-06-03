16e41ad94bffd33f49de6ca426b510445a8de47d
e <kksk0530@naver.com>
2024-06-04 01:09:31 +0900

    feat 인터페이스 주석 추가 #1

--- IEarthWorm.js
+++ IEarthWorm.js
@@ -1,51 +1,88 @@
+/*
+ * 지렁이 게임 인터페이스 입니다.
+ * @author kksk0530, jiyoung0822
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 class IEarthWorm {
+    /*
+     * 객체 기본 생성자 입니다.
+     * 맵의 크기를 기본값으로 받아와 설정합니다.
+     * width : 맵 너비 (int) 
+     * height : 맵 높이 (int)
+     * headDirection : 지렁이 방향 (String)
+     * worm : 지렁이 좌표 (Object)
+     * apple : 사과 자표 (Object)
+     * isGameOver : 게임오버 여부 (boolean)
+     */
     constructor(width, height) {
-        this.width = width;                 // 맵 너비값
-        this.height = height;              // 맵 높이값
-        this.headDirection = 'RIGHT';  // 초기 방향
-        this.worm = {x:10,y:10};          //지렁이 좌표
-        this.apple = '';                     //사과 좌표
-        this.isGameOver = false;       // 종료 여부
-    }
-
-    gameStart(){
-        //게임을 실행하는 함수
-    }
-  
-    setHeadDirection(direction){
-        //지렁이의 진행 방향 설정
+        this.width = width;
+        this.height = height;
+        this.headDirection = 'RIGHT';
+        this.worm = {x:10,y:10};
+        this.apple = '';
+        this.isGameOver = false;
     }
     
-    //라이브러리에서 사용
+    /*
+     * 재시작 시 게임을 초기화 하는 함수입니다.
+     * headDirection, worm, apple, isGameOver 초기화 합니다.
+     */
+    gamerReset(){
+
+    }
+
+    /*
+     * 지렁이 진행 방향을 설정합니다.
+     */
+    setHeadDirection(direction){
+
+    }
+    
+    /*
+     * 지렁이가 먹을 사과가 생성될 좌표값을 랜덤으로 설정합니다.
+     * 라이브러리에서 apple 인스턴스 변수 좌표 값을 설정할 때 사용합니다.
+     */
     createApple() {
-        //사과를 좌표를 랜덤으로 생성하는 함수
+
     }
 
+    /*
+     * 지속적인 호출을 통해
+     * GameOver 여부 확인 후 인스턴스 객채 값 설정
+     * headDirection 인스턴스 객체 방향 값으로 지렁이 좌표 갱신
+     * 지렁이 사과 섭취 시 worm 길이 증가 및 애플 객채 랜덤 좌표 생성
+     * 방향 값으로 지렁이 좌표가 변경 됩니다.
+     */
     updateWormStatus() {
-        //지렁이의 상태를 지속적으로 갱신하기 위한 함수
-        // 1. 키보드 클릭에 따른 방향 갱신
-        // 2. 음식 섭취 여부에 따른 길이 갱신
-        // 3. 지렁이 이동 좌표 갱신
-        // 4. 지렁이 생사여부 갱신
+
     } 
-  
+    
+    /* 
+     * 지렁이의 좌표 값을 반환합니다.
+     */
     getWorm(){
-        //화면에 출력할 지렁이 좌표
-        return this.direction;
+        return this.worm;
     }
 
+    /* 
+     * 사과의 좌표 값을 반환합니다.
+     */
     getApple(){
-        //화면에 출력할 음식 좌표
         return this.apple;
     }
 
+    /* 
+     * 게임 종료 여부를 반환합니다.
+     */
     getIsGameOver(){
-         //화면에 출력할 게임 종료 여부
          return this.isGameOver;
     }
-
+    
+    /* 
+     * 지렁이의 진행 방향 값을 반환합니다.
+     */
     getHeadDirection(){
-        //화면에 출력할 진행 방향 정보
         return this.headDirection;
     }
 }
